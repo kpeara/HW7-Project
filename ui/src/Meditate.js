2873f78a85297df03dc314a618321b44c7006dba
Kunju Perath <kperath@gmail.com>
2020-11-21 16:30:40 -0500

    removed pages we probably wont have time to work on

--- ui/src/Meditate.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Meditate() {
-    return <h1>Meditate</h1>;
-}
-
-export default Meditate;
--- ui/src/Vent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Vent() {
-    return <h1>Vent</h1>;
-}
-
-export default Vent;
