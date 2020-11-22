2df99adb11fd49c8049516c68e6c3e35b9f0896b
Kunju Perath <kperath@gmail.com>
2020-11-21 22:27:54 -0500

    users can be stored in db now

--- ws/src/main/java/com/hackwestern/ws/MainController.java
+++ ws/src/main/java/com/hackwestern/ws/MainController.java
@@ -8,15 +8,12 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class MainController {
 
+    @Autowired
     private UserRepository userRepository;
 
     @PostMapping(path="/login")
-    public @ResponseBody String addNewUser (@RequestParam String username, @RequestParam String password) {
-
-        User n = new User();
-        n.setUsername(username);
-        n.setPassword(password);
-        userRepository.save(n);
-        return "Saved"; // TODO: return response entity (200 ok)
+    public @ResponseBody String addNewUser (@RequestBody User user) {
+        userRepository.save(user);
+        return "Saved";
     }
 }
