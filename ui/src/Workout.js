4b558df3651efc2660c25ad787cef89c6509aa64
Kunju Perath <kperath@gmail.com>
2020-11-21 23:36:54 -0500

    trying to add dropdown

--- ui/src/Workout.js
+++ ui/src/Workout.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 function Workout() {
-    return <h1>Workout</h1>;
+    return (
+        <>
+            <div className="container mt-4">
+                <div className="dropdown is-active">
+                    <div className="dropdown-trigger">
+                        <button
+                            className="button"
+                            aria-haspopup="true"
+                            aria-controls="dropdown-menu"
+                        >
+                            <span>Select Workout</span>
+                            <span className="icon is-small">
+                                <i
+                                    className="fas fa-angle-down"
+                                    aria-hidden="true"
+                                ></i>
+                            </span>
+                        </button>
+                    </div>
+                    <div
+                        className="dropdown-menu"
+                        id="dropdown-menu"
+                        role="menu"
+                    >
+                        <div className="dropdown-content">
+                            <a className="dropdown-item">Dropdown item</a>
+                            <a className="dropdown-item">Other dropdown item</a>
+                            <a href="#" className="dropdown-item">
+                                Active dropdown item
+                            </a>
+                            <a href="#" className="dropdown-item">
+                                Other dropdown item
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Workout;
