3a1b4f4cd4e007291b4780faf5ada91eee246c08
Arrian Gibson-Khademi <agibsonk@uwo.ca>
2020-11-22 00:21:34 -0500

    Venting fading in and out working, need to fix css

--- ui/src/Vent.js
+++ ui/src/Vent.js
@@ -1,20 +1,39 @@
 import React, { Component } from "react";
 
+import "./vent.css";
+
+const FADE_DURATION = 1000;
+
 class Vent extends Component {
     constructor(props) {
         super(props)
-        this.state = { value: '' };
+        this.state = {
+            value: '',
+
+            fadeTransition: null,
+            fadeState: "fade-in"
+        };
 
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleBurnSubmit = this.handleBurnSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({ value: event.target.value });
+        event.preventDefault();
+        this.setState({
+            fadeState: "fade-in",
+            value: event.target.value
+        });
     }
 
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+    handleBurnSubmit(event) {
+        alert('the burn it button was pressed with text: ' + this.state.value);
+        // Create a timer that runs after quote
+        // fades out
+        this.setState({
+            fadeState: "fade-out",
+            value: ''
+        })
         event.preventDefault();
     }
 
@@ -23,12 +42,21 @@ class Vent extends Component {
         return (
             <div>
                 <h1>Feel free to vent, type about what has been concerning you lately, and then throw it away!</h1>
-                <p>Your worries: {this.state.value}</p>
-                <form onSubmit={this.handleSubmit}>
+                <div
+                    className={`fade-wrapper ${this.state.fadeState}`}
+                    style={{ transitionDuration: `${FADE_DURATION}ms` }}
+                >
+                    <p>Your note: </p>
+                    <div class="note">
+                        {this.state.value}
+                    </div>
+
+                </div>
+                <form onSubmit={this.handleBurnSubmit}>
                     <p><input type='text' placeholder='Type your worries here' value={this.state.value} onChange={this.handleChange} /></p>
                     <p><input type='submit' value='Burn it!' /></p>
                 </form>
-            </div>
+            </div >
         )
 
     }
